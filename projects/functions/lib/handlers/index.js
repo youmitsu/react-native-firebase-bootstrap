18f3cda01d2217a41e7dc9712414e41bd521e0a5
youmitsu <holly.app.dev@gmail.com>
2022-12-30 15:58:27 +0900

    fix gitignore

--- projects/functions/lib/handlers/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.hello = void 0;
-var hello_1 = require("./hello");
-Object.defineProperty(exports, "hello", { enumerable: true, get: function () { return hello_1.hello; } });

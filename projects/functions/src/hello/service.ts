3e9074dffdedc0c3ffc59800cd7faae2c0071313
youmitsu <holly.app.dev@gmail.com>
2023-01-02 15:19:55 +0900

    fix: ci

--- projects/functions/src/hello/service.ts
+++ projects/functions/src/hello/service.ts
@@ -1,9 +1,22 @@
-import * as Types from "rnfb-bootstrap-schema";
+import { firestore } from "firebase-admin";
+import { Firestore } from "rnfb-bootstrap-schema";
 
-export const service = (): boolean => {
-  const user: Types.Firestore.User.Schema = {
+export const service = async (): Promise<boolean> => {
+  const user: Firestore.User.Schema = {
     name: "bobby",
   };
   console.log("hello world", user);
+
+  const snapshot = await firestore()
+    .collection("users")
+    .withConverter(Firestore.User.Converter)
+    .doc("hoge")
+    .get();
+  const data = snapshot.data();
+  console.log("data exists", snapshot.exists);
+  if (data !== undefined) {
+    console.log("name", data.name);
+  }
+
   return true;
 };
--- projects/schema/src/firestore/user.ts
+++ projects/schema/src/firestore/user.ts
@@ -1,22 +1,17 @@
-import * as firestore from "firebase/firestore";
+import * as firebase from "firebase-admin";
 import { z } from "zod";
-
-import type { FirestoreDataConverter } from "firebase/firestore";
 
 const _User = z.object({
   name: z.string(),
 });
 export type Schema = z.infer<typeof _User>;
 
-export const Converter: FirestoreDataConverter<Schema> = {
-  toFirestore: (user: Schema): firestore.DocumentData => {
+export const Converter: firebase.firestore.FirestoreDataConverter<Schema> = {
+  toFirestore: (user: Schema): firebase.firestore.DocumentData => {
     return { name: user.name };
   },
-  fromFirestore: (
-    snapshot: firestore.QueryDocumentSnapshot,
-    options?: firestore.SnapshotOptions
-  ) => {
-    const data = snapshot.data(options);
+  fromFirestore: (snapshot: firebase.firestore.QueryDocumentSnapshot) => {
+    const data = snapshot.data();
     const user: Schema = {
       name: data.name,
     };
